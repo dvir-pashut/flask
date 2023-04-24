fa57c4eb60d2c6f51f07685718acef84a9664fe1
dvir pashut <72396086+dvir-pashut@users.noreply.github.com>
2023-04-24 16:57:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 @app.route('/elior')
-def index():
+def elior():
     return jsonify({"elior is ugly"})
 
 
